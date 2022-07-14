9d379ed4eda1067e3304459955f38ede7ce81579
Mateusz Krawczyk <mateusz.krawczyk@green-code.studio>
2022-07-14 19:02:22 +0200

    .show procs as clsoed

--- src/SubTask.js
+++ src/SubTask.js
@@ -1,4 +1,5 @@
 import {Buffer} from "./Buffer.js";
+import {log} from "./debug.js";
 
 export class SubTask {
     constructor(proc, name, output = {}, onchange = null) {
@@ -16,6 +17,7 @@ export class SubTask {
         proc.stdout.setEncoding('utf8');
 
         proc.on('close', (code) => {
+            log('closeProc')
             this.status = 'closed';
             this.draw();
             this.onchange();
@@ -72,7 +74,7 @@ export class SubTask {
         this.output.stream.write('\x1B[4m');
         let position = 0;
 
-        while (position < (this.output.width - this.name.length) / 2) {
+        while (position < (this.output.width - name.length) / 2) {
             this.output.stream.write(' ');
             position++;
         }
@@ -81,7 +83,7 @@ export class SubTask {
             if (!this.isInside)
                 this.output.stream.write('\x1B[7m');
         }
-        this.output.stream.write(this.name);
+        this.output.stream.write(name);
         if (this.isCurrentTask) {
             if (!this.isInside)
                 this.output.stream.write('\x1B[27m');
--- src/debug.js
+++ src/debug.js
@@ -1,5 +1,5 @@
 import {promises as fs} from 'fs'
 
 export function log(data){
-   // fs.writeFile('./debug.txt', new Date()+': '+data+'\r\n\r\n', {encoding:'UTF8', flag:'a'})
+    //fs.writeFile('./debug.txt', new Date()+': '+data+'\r\n\r\n', {encoding:'UTF8', flag:'a'})
 }
